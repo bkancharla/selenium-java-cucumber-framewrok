85afa590c89a39389a14bdbd0c77998a1e8f402f
bkancharla <bkancharla@lb.com>
2018-12-08 19:35:11 -0500

    Starting chrome Driver

--- src/test/java/RunCukesTest.java
+++ src/test/java/RunCukesTest.java
@@ -1,6 +1,15 @@
+import com.automation.steps.TestSteps;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.log4j.Logger;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.DataProvider;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 @CucumberOptions(
         features = "src/test/resources/features",
@@ -12,7 +21,25 @@ public class RunCukesTest extends AbstractTestNGCucumberTests {
     @DataProvider(parallel = true)
     @Override
     public Object[][] scenarios() {
-        System.out.println("DDUE");
         return super.scenarios();
+    }
+    private static Logger log = Logger.getLogger(TestSteps.class);
+
+    @AfterSuite
+    public static void generateReport() {
+        //	https://github.com/damianszczepanik/cucumber-reporting
+        log.info("......Generating the Report....");
+        File reportOutputDirectory = new File("target/masterthought");
+
+        List<String> jsonFiles = new ArrayList<>();
+        File e = new File("target/jsonReports/1.json");
+        Configuration configuration = new Configuration(reportOutputDirectory, "ETAF");
+
+        configuration.addClassifications("Platform", "Windows");
+        configuration.addClassifications("Browser", "chrome");
+        jsonFiles.add(e.getAbsolutePath());
+        log.info("......Generating the Report in process....");
+
+        new ReportBuilder(jsonFiles, configuration).generateReports();
     }
 }
